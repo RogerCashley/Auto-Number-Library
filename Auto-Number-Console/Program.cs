439d6016fd8782e8602e6d4d681f811f027ecea3
Roger Cashley <chill.chill5657@gmail.com>
2022-06-15 11:00:34 +0700

    Added console application for testing.

---
+++ Auto-Number-Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NetFramework.Library;
+
+namespace Auto_Number_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString1 = "INV/001";
+            string testString2 = "INV/001A";
+            string testString3 = "INV/999A";
+
+            Console.WriteLine(testString1.Add(1)); // INV/002
+            Console.WriteLine(testString2.Add(1)); // INV/002A
+            Console.WriteLine(testString3.Add(1)); // INV/1000A
+
+            Console.ReadKey();
+        }
+    }
+}
